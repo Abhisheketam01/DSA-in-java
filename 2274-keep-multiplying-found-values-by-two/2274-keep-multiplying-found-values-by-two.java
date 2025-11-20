0af4ff71af5169000afe5ef512672d9a43a28918
ABHISHEK-B <etamabhishek000@gmail.com>
2025-11-20 06:08:12 +0530

    Update 2274-keep-multiplying-found-values-by-two.java

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -17,4 +17,20 @@ class Solution {
 }
 
 solution in c++
+    class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        //Syntax: std::set <data_type> set_name;
+        unordered_set<int> s;
+        // first store every element into set from array good.
+        for(int i = 0; i < nums.size(); i++){
+            s.insert(nums[i]);
+        }
+        //
+        while(s.find(original) != s.end()){
+            original = original * 2;
+        }
+        return original;        
+    }
+};
     
