1a1f82a18b8b5dab3f03b8d8b0ab58185f3607aa
ABHISHEK-B <etamabhishek000@gmail.com>
2025-12-26 04:22:24 +0530

    Time: 0 ms (100%), Space: 28.4 MB (5.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -17,15 +17,26 @@
 class Solution {
 public:
     void reverseString(vector<char>& s){
-        int left = 0;
-        int right = s.size()-1;
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
+        // int left = 0;
+        // int right = s.size()-1;
+        // while(left < right){
+        //     char temp = s[left];
+        //     s[left] = s[right];
+        //     s[right] = temp;
 
-            left++;
-            right--;
-        }
+        //     left++;
+        //     right--;
+            int n = s.size();
+        // Temporary array to store elements 
+        // in reversed order
+        vector<int> temp(n);
+        // Copy elements from original array 
+        // to temp in reverse order
+        for(int i = 0; i < n; i++)
+        temp[i] = s[n - i - 1];
+  
+        // Copy elements back to original array
+        for(int i = 0; i < n; i++)
+        s[i] = temp[i];
     }
 };
