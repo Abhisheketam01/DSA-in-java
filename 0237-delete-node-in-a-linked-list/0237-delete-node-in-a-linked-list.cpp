90bf84aeef55ed604a48e0f2924ce0297defcc99
ABHISHEK-B <etamabhishek000@gmail.com>
2026-01-09 16:05:53 +0530

    Time: 3 ms (95.4%), Space: 12.2 MB (97.51%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -1,18 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        // Definition of a singly-linked list node (provided by LeetCode)
-        node->val = node->next->val;
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+//         // Definition of a singly-linked list node (provided by LeetCode)
+//         node->val = node->next->val;
 
-        // Step 2: Skip the next node by pointing to the node after next
-        node->next = node->next->next;  
+//         // Step 2: Skip the next node by pointing to the node after next
+//         node->next = node->next->next;  
+//     }
+// };
+
+// 1st time
+/*
+Definition for singly linked list
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+*/
+
+
+
+class Solution {
+public :
+    void deleteNode(ListNode* node){
+        node->val = node-> next->val;
+        node->next = node->next->next;
+
     }
 };
