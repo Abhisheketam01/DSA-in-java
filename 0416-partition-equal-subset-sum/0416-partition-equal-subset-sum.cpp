6cf84664121b2e924ac22df155cac05ca5864001
ABHISHEK-B <etamabhishek000@gmail.com>
2026-01-11 21:12:51 +0530

    Time: 84 ms (75.15%), Space: 13.4 MB (85.57%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,100 @@
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int n = nums.size();
+//         int total = 0;
+//         for (int i = 0; i < n; i++) {
+//             total += nums[i];
+//         }
+//         if (total % 2 != 0) {
+//             return false;
+//         }
+//         int target = total / 2;
+//         vector<bool> dp(target + 1, false);
+//         dp[0] = true;
+//         for (int num : nums) {
+//             for (int s = target; s >= num; s--) {
+//                 dp[s] = dp[s] || dp[s - num];
+//             }
+//         }
+//         return dp[target];
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
+public : 
+    bool canPartition(vector<int>& nums){
         int n = nums.size();
         int total = 0;
-        for (int i = 0; i < n; i++) {
-            total += nums[i];
+        for(int i = 0; i < n; i++){
+            total = total + nums[i];
         }
-        if (total % 2 != 0) {
+        if(total % 2 != 0){
             return false;
         }
         int target = total / 2;
+        // how to create an array using target
         vector<bool> dp(target + 1, false);
         dp[0] = true;
-        for (int num : nums) {
-            for (int s = target; s >= num; s--) {
+        for(int num : nums){
+            for(int s = target ; s>=num ; s--){
                 dp[s] = dp[s] || dp[s - num];
             }
         }
         return dp[target];
+        /*
+        🟢 Core Trick (MOST IMPORTANT IDEA)
+
+                If:
+                totalSum = S
+                If you can find ONE subset with sum = S/2
+                Then:
+                the remaining elements automatically sum to S/2
+                💥 BOOM — second subset is implicit.
+                        num = is value 
+                🔵 What dp[x] = true REALLY means
+
+                    Say it out loud:
+                    “YES, I can make sum x using some of the numbers.”
+                    If dp[x] = false:
+                    “Nope, impossible so far.”
+                dp[s] = dp[s] || dp[s - num]
+
+
+
+        */
     }
+
 };
