a74e2f03c437fcf980144d011d23ab909c4622ce
ABHISHEK-B <etamabhishek000@gmail.com>
2026-01-12 02:16:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (25.62%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -6,6 +6,33 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[n][m] = true;
+        for (int i = n; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                bool firstMatch = (i < n && (s[i] == p[j] || p[j] == '.'));
+                if (j + 1 < m && p[j + 1] == '*') {
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                }
+                else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
 
         // Step 1: create dp table
         // dp[i][j] = can s[i..n-1] match p[j..m-1]?
@@ -38,3 +65,5 @@ public:
         return dp[0][0];
     }
 };
+
+*/
